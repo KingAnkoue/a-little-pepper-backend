91892902e4784e847e7c2a1d1a96fa78625e2040
King Ankoue <thierryankoue@pursuit.com>
2022-12-20 19:56:49 -0500

    firebase and deployment

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const { PG_DATABASE, DATABASE_URL, PG_HOST, PG_PASSWORD, PG_USER, PG_PORT } = process.env;
 console.log(PG_DATABASE, PG_HOST, PG_PASSWORD, PG_PORT, PG_USER, DATABASE_URL);
-console.log( " %$^%#^$%^&%$&$$%&$%& this is the stuff fromt the db config $%&%$&$%&$%&&$%&");
+console.log( " %$^%#^$%^&%$&$$%&$%& this is the stuff from the db config $%&%$&$%&$%&&$%&");
 
 const cn = DATABASE_URL
   ? {
